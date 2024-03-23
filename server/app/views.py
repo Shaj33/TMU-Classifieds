ba122104fcb61116bf10b859a7271decc8954b14
jemilm123 <jcmodha@yahoo.ca>
2024-03-23 18:36:05 -0400

    moving dummy data to resetDB script

--- server/app/views.py
+++ server/app/views.py
@@ -18,51 +18,18 @@ dbname = client['mainDB']
 
 #Define Collection
 collection = dbname['User']
-collection2 = dbname['Ads']
 
 mascot_1={
     'id': 1,
     "Name": "Sammy"
 }
 
-ad_1={
-    'id': 1,
-    'user_id': 1,
-    'type': 'Wanted',
-    'title': 'Looking for textbook',
-    'content': 'looking for Intro to CS first edition',
-    'location': 'Toronto'
-}
 
-ad_2={
-    'id': 2,
-    'user_id': 1,
-    'type': 'For Sale',
-    'title': 'Selling calculator',
-    'content': 'I want to get rid of my calculator',
-    'location': 'Toronto',
-    'price': 5
-}
-
-ad_3={
-    'id': 3,
-    'user_id': 1,
-    'type': 'Services',
-    'title': 'Offering tutoring services',
-    'content': 'Willing to teach math and computer science',
-    'location': 'Toronto',
-    'price': 15
-}
 
 
 collection.insert_one(mascot_1)
-collection2.insert_many([ad_1, ad_2, ad_3])
 
 user_details = collection.find({})
-ad_details = collection2.find({})
 
 for r in user_details:
     print(r['Name'])
-
-for ad in ad_details:
-    print(ad)
